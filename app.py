aa17a9e5333312d9577fae5131ee49ab9cffda81
tanmay_korate <koratetanmay@gmail.com>
2025-10-04 00:12:28 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+    return render_template('success.html', name=name, email=email, message=message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
